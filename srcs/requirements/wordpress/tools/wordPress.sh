5996ad42295d684f1def92bdbd74ef6cef191e77
CodeAndCofe <adilprogrammer73@gmail.com>
2026-04-16 14:21:15 +0200

    v2.02

--- srcs/requirements/wordpress/tools/wordPress.sh
+++ srcs/requirements/wordpress/tools/wordPress.sh
@@ -3,6 +3,18 @@
 set -e
 
 echo "WordPress container starting..."
+ROOT_PASSWORD=$(cat ./secrets/db_root_password.txt)
+DB_DATABASE="${MYSQL_DATABASE}"
+DB_USER="${MYSQL_USER}"
+DB_PASSWORD=$(cat ./secrets/db_password.txt)
+touch   wp-config.php
 
-
+cat > /var/www/html/wp-config.php <<EOF
+<?php
+    define('DB_NAME', $DB_DATABASE);
+    define('DB_USER', $DB_USER);
+    define('DB_PASSWORD', $ROOT_PASSWORD);
+    define('DB_HOST', 'localhost:3306');
+?>
+EOF
 exec php-fpm8.2 -F
