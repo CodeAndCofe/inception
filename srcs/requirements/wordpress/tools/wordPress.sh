8c50cdebdfd9fcc5a28423d570a0eaa5cb357cd7
adilferryat <adilprogrammer73@gmail.com>
2026-04-29 18:15:48 +0000

    ok

--- srcs/requirements/wordpress/tools/wordPress.sh
+++ srcs/requirements/wordpress/tools/wordPress.sh
@@ -2,10 +2,31 @@
 WP_USER="adil"
 DB_USER="adil"
 DB_PASS="pass"
+DB_NAME="wp"
+DB_HOST="mariadb"
+WP_URL=https://aferryat.42.ft.com
+WP_TITLE=Blog
+WP_ADMIN_USER=aferryat
+WP_ADMIN_PASSWORD=33333333
+WP_ADMIN_EMAIL=adilprogrammer@gmail.com
 
-
-WP=php wp-cli.phar
-
+cd /var/www/html
 
 $(WP) core download
 
+if [! -f wp-config.php] then;
+
+wp config create \
+  --dbname=$(DB_NAME) \
+  --dbuser=$(DB_USER) \
+  --dbpass=$(DB_PASS) \
+  --dbhost=$(mariadb)
+
+wp core install \
+  --url=$(WP_URL) \
+  --title=$(WP_TITLE)\
+  --admin_user=$(WP_ADMIN_USER) \
+  --admin_password=$(WP_ADMIN_PASSWORD) \
+  --admin_email=$(WP_ADMIN_EMAIL)
+
+fi
