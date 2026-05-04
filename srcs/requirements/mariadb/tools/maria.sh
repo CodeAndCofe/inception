a22228f81fde9bcc693192a03427e45e2835d8c2
CodeAndCofe <adilprogrammer73@gmail.com>
2026-05-04 20:24:38 +0200

    w

--- srcs/requirements/mariadb/tools/maria.sh
+++ srcs/requirements/mariadb/tools/maria.sh
@@ -1,4 +1,4 @@
-# #!/bin/bash
+#!/bin/bash
 set -e
 
 echo "Starting MariaDB setup..."
@@ -6,42 +6,36 @@ echo "Starting MariaDB setup..."
 ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 DB_PASSWORD=$(cat /run/secrets/db_password)
 
+
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
-# mysqld --user=mysql
-
-# sleep 5
-
-
+# If database not initialized, run bootstrap
 if [ ! -f "/var/lib/mysql/.initialized" ]; then
-    echo "Initializing database..."
-    echo "starting mariadb"
-    mysqld  -u root  <<EOF
+    echo "Initializing database (offline)..."
+
+    # Create system tables (if not already present)
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql >/dev/null
+
+    # Use --bootstrap to run SQL without a running server
+    echo "creating user ...."
+    mariadbd --bootstrap --user=mysql <<EOF
 USE mysql;
 FLUSH PRIVILEGES;
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS ${DB_NAME};
 CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
+
     touch /var/lib/mysql/.initialized
     echo "Database initialized."
 else
     echo "Database already initialized, skipping."
 fi
 
-# mysqladmin -u root -p"${ROOT_PASSWORD}" shutdown
-
 echo "Starting MariaDB in foreground..."
 
-exec mysqld --user=mysql --bind-address=0.0.0.0 --port=3306
-
-
-
-check the admin and check sql 
-
-!/bin/sh
-
+exec mariadbd --user=mysql --bind-address=0.0.0.0 --port=3306 --socket=/run/mysqld/mysqld.sock
